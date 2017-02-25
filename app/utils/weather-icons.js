de041b186830f07f2282b273a6001c0661d91a7a
Ryan Coughlin <ryan@ryancoughlin.com>
2017-02-25 16:29:35 -0500

    Add in fog weather icon

--- app/utils/weather-icons.js
+++ app/utils/weather-icons.js
@@ -3,6 +3,7 @@ import partlySunny from '../assets/images/weather/partly-sunny.png'
 import cloudy from '../assets/images/weather/cloudy.png'
 import rain from '../assets/images/weather/rainy.png'
 import wind from '../assets/images/weather/wind.png'
+import fog from '../assets/images/weather/fog.png'
 
 const weatherTypes = {
   'clear-day': sun,
@@ -11,6 +12,7 @@ const weatherTypes = {
   'cloudy': cloudy,
   'partly-cloudy-day': partlySunny,
   'partly-cloudy-night': partlySunny,
+  'fog': fog,
 }
 
 export default function weatherIcon(weather) {
