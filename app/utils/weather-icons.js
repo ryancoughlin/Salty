4a4355da3d5a28479ae481ca810358321928e957
Ryan Coughlin <ryan@ryancoughlin.com>
2017-03-14 12:57:15 -0400

    Add snow icon

--- app/utils/weather-icons.js
+++ app/utils/weather-icons.js
@@ -1,20 +1,22 @@
-import sun from '../assets/images/weather/sunny.png'
-import partlySunny from '../assets/images/weather/partly-sunny.png'
-import cloudy from '../assets/images/weather/cloudy.png'
-import rain from '../assets/images/weather/rainy.png'
-import wind from '../assets/images/weather/wind.png'
-import fog from '../assets/images/weather/fog.png'
+import sunIcon from '../assets/images/weather/sunny.png'
+import partlySunnyIcon from '../assets/images/weather/partly-sunny.png'
+import cloudyIcon from '../assets/images/weather/cloudy.png'
+import rainIcon from '../assets/images/weather/rainy.png'
+import windIcon from '../assets/images/weather/wind.png'
+import fogIcon from '../assets/images/weather/fog.png'
+import snowIcon from '../assets/images/weather/snow.png'
 
 const weatherTypes = {
-  'clear-day': sun,
-  'wind': wind,
-  'rain': rain,
-  'cloudy': cloudy,
-  'partly-cloudy-day': partlySunny,
-  'partly-cloudy-night': partlySunny,
-  'fog': fog,
+  'clear-day': sunIcon,
+  'partly-cloudy-day': partlySunnyIcon,
+  'partly-cloudy-night': partlySunnyIcon,
+  wind: windIcon,
+  rain: rainIcon,
+  cloudy: cloudyIcon,
+  fog: fogIcon,
+  snow: snowIcon,
 }
 
 export default function weatherIcon(weather) {
-  return weatherTypes[weather] || sun
+  return weatherTypes[weather] || sunIcon
 }
