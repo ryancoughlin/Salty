dda7f0b90f836b63088b7d1711547b435b12a5df
Ryan Coughlin <ryan@ryancoughlin.com>
2017-06-05 08:45:40 -0400

    Fix logic for finding current tide direction [broken]

--- app/utils/find-tide-direction.js
+++ app/utils/find-tide-direction.js
@@ -1,9 +1,13 @@
 import _ from 'lodash'
+import moment from 'moment'
 
 export default function findTideDirection(todaysTides) {
-  const futureTides = _.filter(todaysTides, tide => !tide.pastTide)
+  const now = moment()
+  const isPastTide = tide => moment.utc(tide.time).local().diff(now, 'minutes') < 0
+  const futureTides = _.filter(todaysTides, tide => !isPastTide(tide))
+  const nextTide = futureTides[0].tide
 
-  if (futureTides[0] === 'high') {
+  if (nextTide === 'high' || futureTides.length === 0) {
     return _.upperFirst('incoming')
   } else {
     return _.upperFirst('outgoing')
