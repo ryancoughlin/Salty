a084e0c17dbe195b52ed3897224eea72032e573d
Ryan Coughlin <ryan@ryancoughlin.com>
2017-07-22 14:31:17 -0400

    Add duration to redux logger options

--- app/store.js
+++ app/store.js
@@ -8,7 +8,7 @@ const middlewares = [thunk]
 
 if (process.env.NODE_ENV === 'development') {
   const { createLogger } = require('redux-logger')
-  const logger = createLogger()
+  const logger = createLogger({ duration: true })
   middlewares.push(logger)
 }
 
