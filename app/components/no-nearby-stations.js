e59c0f6a9fe2b7fe831df9d42d01641366f3ae8a
Ryan Coughlin <ryan@ryancoughlin.com>
2017-07-22 14:30:43 -0400

    Update logic within no nearby stations component

--- app/components/no-nearby-stations.js
+++ app/components/no-nearby-stations.js
@@ -1,20 +1,43 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { StyleSheet, View, Text, TouchableOpacity, Image } from 'react-native'
+import Map from './map'
+import SaltyModal from './modal'
 
 import BaseStyle from '../base-styles'
 import smallStationMark from '../assets/images/no-stations-nearby/small-mark.png'
 
-const NoNearbyStations = ({ city, navigation }) =>
-  (<View style={styles.container}>
-    <View>
-      <Image source={smallStationMark} style={styles.smallMark} />
-      <Text style={styles.notFoundHeader}>Oops, we can't find a station near {city}</Text>
-      <TouchableOpacity onPress={() => navigation.navigate('Map')}>
-        <Text style={styles.findStationText}>Find stations</Text>
-      </TouchableOpacity>
-    </View>
-  </View>)
+const NoNearbyStations = class extends Component {
+  constructor(props) {
+    super(props)
 
+    this.state = {
+      mapVisible: false,
+    }
+  }
+
+  render() {
+    const { mapVisible } = this.state
+    const { city } = this.props
+
+    return (
+      <View style={styles.container}>
+        <View>
+          <Image source={smallStationMark} style={styles.smallMark} />
+          <Text style={styles.notFoundHeader} allowFontScaling={false}>Oops, we can&apos;t find a station near {city}</Text>
+          <TouchableOpacity onPress={() => this.setState({ mapVisible: !mapVisible })}>
+            <Text style={styles.findStationText} allowFontScaling={false}>Find stations</Text>
+          </TouchableOpacity>
+        </View>
+        <SaltyModal
+          visible={mapVisible}
+          dismissModal={() => this.setState({ mapVisible: !mapVisible })}
+        >
+          <Map dismissModal={() => this.setState({ mapVisible: !mapVisible })} />
+        </SaltyModal>
+      </View>
+    )
+  }
+}
 const styles = StyleSheet.create({
   container: {
     alignItems: 'flex-end',
