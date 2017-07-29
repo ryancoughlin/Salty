707b8b416402300cb8ed1665ad58512d0bed4db3
Ryan Coughlin <ryan@ryancoughlin.com>
2017-07-28 20:59:09 -0400

    Remove unused moment variable

--- app/utils/find-next-tide.js
+++ app/utils/find-next-tide.js
@@ -1,13 +1,11 @@
 import _ from 'lodash'
 import moment from 'moment'
 
-const now = moment()
-
 export default function findNextTide(tides) {
   const allTides = _.flatten(_.values(tides))
   const nextTideIndex = _.findIndex(allTides, tide => {
     const tideTime = moment.utc(tide.time).local()
-    return now.diff(tideTime) <= 0
+    return moment().diff(tideTime) <= 0
   })
 
   return allTides[nextTideIndex]
