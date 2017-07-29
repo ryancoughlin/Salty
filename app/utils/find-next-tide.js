73318d8fff141be336350bc09b1ba10d0383162b
Ryan Coughlin <ryan@ryancoughlin.com>
2017-07-28 20:52:56 -0400

    Make find next time work smarter not harder

--- app/utils/find-next-tide.js
+++ app/utils/find-next-tide.js
@@ -1,22 +1,14 @@
 import _ from 'lodash'
 import moment from 'moment'
 
-const DATE_FORMAT = 'MM/DD/YYYY'
 const now = moment()
 
 export default function findNextTide(tides) {
-  const todayKey = now.format(DATE_FORMAT)
-  const todaysTides = tides[todayKey]
-
-  const nextTideIndex = _.findIndex(todaysTides, tide => {
+  const allTides = _.flatten(_.values(tides))
+  const nextTideIndex = _.findIndex(allTides, tide => {
     const tideTime = moment.utc(tide.time).local()
     return now.diff(tideTime) <= 0
   })
 
-  if (nextTideIndex === -1) {
-    const tomorrowKey = now.add(1, 'days').format(DATE_FORMAT)
-    return tides[tomorrowKey][0]
-  }
-
-  return todaysTides[nextTideIndex]
+  return allTides[nextTideIndex]
 }
