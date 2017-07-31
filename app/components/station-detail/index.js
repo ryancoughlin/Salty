2c1f631cf24f2c435af5b6f17de98ce9a8e53187
Ryan Coughlin <ryan@ryancoughlin.com>
2017-07-31 13:26:51 -0400

    Misc formatting and removing old code

--- app/components/station-detail/index.js
+++ app/components/station-detail/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { StyleSheet, ScrollView, View } from 'react-native'
+import { StyleSheet, ScrollView } from 'react-native'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 
